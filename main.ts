9946a6869c775323ec2eb0c192cc052047ecea75
JasonCammarato <122560123+JasonCammarato@users.noreply.github.com>
2023-01-23 13:27:29 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,240 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . # . #
+        . . . . .
+        # . # # #
+        # . . . .
+        . # . . .
+        `)
+})
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showLeds(`
+        . # . # .
+        # . . . #
+        . # # # .
+        . . . . .
+        . . . . .
+        `)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showLeds(`
+        . . # . .
+        . . . . #
+        . . . . #
+        . # . # .
+        . . . # .
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        # # # # #
+        . . . . .
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        # . # . .
+        . . . . .
+        # # # . #
+        . . . . #
+        . . . # .
+        `)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # # .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # # .
+        . . . # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # # .
+        . . . # .
+        . . . # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . # .
+        . . . # .
+        . . # # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . # .
+        . # # # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        # # # # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . .
+        # # # . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # . . . .
+        # . . . .
+        # # . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        `)
+    basic.showLeds(`
+        # . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # . . .
+        # . . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # . .
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # # .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . # # # #
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # # #
+        . . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . # #
+        . . . . #
+        . . . . #
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . #
+        . . . . #
+        . . . . #
+        . . . . #
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . #
+        . . . . #
+        . . . . #
+        . . . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . #
+        . . . . #
+        . . . # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . #
+        . . . # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # # # # #
+        . . . . .
+        `)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showLeds(`
+        . . # . .
+        # . . . .
+        # . . . .
+        . # . # .
+        . # . . .
+        `)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # # # .
+        # . . . #
+        . # . # .
+        `)
+})
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . . . .
+    # # # # #
+    . . . . .
+    `)
